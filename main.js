bcc1b576d12df357aa224672cffa6964f7def3d5
Iyke_hermes <31313901+ogumbaiyke@users.noreply.github.com>
2026-07-14 14:04:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -9,4 +9,70 @@ function logCitySkyline() {
 
 console.log(logCitySkyline())
 
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+var satellite = 'The Moon'
+var galaxy = 'The Milky Way'
+var stars = 'North Star'
+
+
+function callMyNightSky() {
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+
+console.log(callMyNightSky())
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+function logVisibleLightWaves() {
+  const lightWaves = 'Moonlight'
+  console.log(lightWaves)
+}
+
+console.log(logVisibleLightWaves());
+console.log(lightWaves)
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+let stars = 'North Star';
+
+const callMyNightSky = () => {
+    stars = 'Sirius'
+
+  
+	return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+};
+
+console.log(callMyNightSky());
+console.log(stars)
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+  const logVisibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+	let region = 'The Arctic';
+  // Add if statement here:
+  if (region === 'The Arctic'){
+    let lightWaves = 'Northern Lights'
+    console.log(lightWaves)
+
+  }
+
+
+  console.log(lightWaves);
+};
+
+logVisibleLightWaves();
+
+
+
+
 
